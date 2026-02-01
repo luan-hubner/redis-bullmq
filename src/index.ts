1f7e07492fa38d6758da10b53adf6a622fdb9746
Luan Hubner <luanhubner.pro@gmail.com>
2026-01-31 21:26:04 -0400

    feat: startup worker on server init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { productsRoutes } from './routes/products.routes';
+import { startWorker } from './infra/queue/product.worker';
 
 export const app = express();
 
 app.use(express.json());
 app.use('/products', productsRoutes);
 
-app.listen(3333, () => {
-  console.log(`server is running on port ${3333}. enjoy! 🚀`);
-});
+function startServer() {
+  app.listen(3333, () => {
+    console.log(`server is running on port ${3333}. enjoy! 🚀`);
+  });
+}
+
+startServer();
+startWorker();

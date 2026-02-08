d19bbcf2614652b8840d41b9181c67c083da4bf2
Luan Hubner <luanhubner.pro@gmail.com>
2026-02-08 14:22:03 -0400

    refactor: move CreateOrder to orders folder

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { adaptRoute } from '../http/adapters/express-route.adapter.js';
-import { createOrderController } from '../application/use-cases/products/create-order/index.js';
+import { createOrderController } from '../application/use-cases/orders/create-order/index.js';
 
 const ordersRoutes = Router();
 

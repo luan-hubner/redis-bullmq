e4398dd9ab5d26de2a395d28468bf67e55741d52
Luan Hubner <luanhubner.pro@gmail.com>
2026-02-08 02:33:02 -0400

    feat: add Orders table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,3 +5,14 @@ export const productsTable = pgTable('products', {
   description: varchar({ length: 255 }).notNull(),
   value: numeric('value', { precision: 5, scale: 2 }).notNull(),
 });
+
+export const ordersTable = pgTable('orders', {
+  id: uuid().primaryKey().defaultRandom(),
+  productId: uuid('product_id')
+    .notNull()
+    .references(() => productsTable.id, {
+      onDelete: 'cascade',
+    }),
+  userEmail: varchar('user_email', { length: 255 }).notNull(),
+  quantity: numeric('quantity', { precision: 5, scale: 2 }).notNull(),
+});
